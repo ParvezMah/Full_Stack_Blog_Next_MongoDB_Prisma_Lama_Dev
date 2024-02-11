9ded1d0f0f9f0116ba53820c7d877dbbf564d955
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-02-12 01:09:12 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return <div>
+            <Link href="/">
+              <h1>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed quo perspiciatis id atque distinctio accusantium. 
+              </h1>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis praesentium eius quidem laboriosam ratione assumenda. Nostrum, quod? Veritatis vitae, iste nisi tempore tenetur quos maiores quisquam odio at illum modi dolore labore perferendis assumenda omnis exercitationem natus impedit aperiam commodi. Rem unde ea fugiat dolorem saepe, amet accusantium corporis adipisci. Nobis, officiis. Repudiandae, quas! Eius facilis corrupti officia non, pariatur minus voluptatem repellendus beatae, ea corporis dolores minima voluptate reprehenderit.
+              </p>
+            </Link>
+        </div>;
+}
