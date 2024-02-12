ef84cfad226c61c44bf545237e0e4ac39b7c2b52
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-02-12 18:01:14 +0600

    CSS Responsiveness Layout set up

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "./Navbar.module.css"
+
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Nabvar section</h1>
+        </div>
+    );
+};
+
+export default Navbar;
